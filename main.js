a97efe47f433f5d19cf12a2cdf5b5e1e46f03b22
David Lau <David.lau@codeimmersives.com>
2020-01-29 12:44:09 -0500

    finished

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
 /******************
  * YOUR CODE HERE *
  ******************/
+const myFirstName = 'David';
+const myLastName = 'Lau';
+const myBestFriend = 'James';
+const mySentence = myFirstName + myLastName + myBestFriend;
+const myFavoriteNum = 6;
+const myMentalAge = 26;
+const feet = 5;
+const inch = 10;
+const myHeight = feet * 12 + inch;
+const myFavoriteYear = 2000;
 
-
- 
+const mySum = 3 + 1 + 1918 + 24;
++ 1
+const myProduct = 232 * 43 / 2 + 4;
+const myHeightAndAge = myMentalAge + myHeight;
 
 
 /*********************************************************************************************************************
